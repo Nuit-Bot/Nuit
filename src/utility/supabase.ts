9be7886dbaf880ca64b437eedd508e7b86fd5284
TotoCode <totocodefr@duck.com>
2026-04-13 11:00:52 +0200

    feat(db): add supabase client utility

---
+++ src/utility/supabase.ts
@@ -0,0 +1,19 @@
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+
+let supabase: SupabaseClient | null = null;
+
+export function getSupabaseClient() {
+    if (!supabase) {
+        const supabaseUrl = process.env.SUPABASE_URL;
+        const supabaseKey = process.env.SUPABASE_KEY;
+
+        if (!supabaseUrl || !supabaseKey) {
+            throw new Error('Supabase URL and Key must be set in environment variables');
+        }
+
+        supabase = createClient(supabaseUrl, supabaseKey, {
+
+        });
+    }
+    return supabase;
+}
