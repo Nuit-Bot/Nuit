38fe09af24393989fff9c89d8674c452cfb765c3
TotoCode <totocodefr@duck.com>
2026-04-25 18:33:30 +0200

    add proper ping command

--- src/modules/main/main.ts
+++ src/modules/main/main.ts
@@ -1,11 +1,21 @@
 import type { ModuleContext } from "@nuit-bot/api";
-import { SlashCommandBuilder } from "discord.js";
+import {
+    MessageFlags,
+    SlashCommandBuilder,
+    ChatInputCommandInteraction,
+} from "discord.js";
+import { cleanMultiline } from "../../discord/utility/cleanMultiline";
 
 export async function setup(ctx: ModuleContext) {
     ctx.api.registerCommand({
         data: new SlashCommandBuilder().setName("ping").setDescription("Pong!"),
-        async execute(interaction) {
-            await interaction.reply({ content: "Pong!" });
+        async execute(interaction: ChatInputCommandInteraction) {
+            await interaction.reply({
+                content: cleanMultiline(`Pong 🏓!
+                Bot's API latency: \`${interaction.client.ws.ping}\`ms
+                General bot latency: \`${Date.now() - interaction.createdTimestamp}\`ms`),
+                flags: MessageFlags.Ephemeral,
+            });
         },
     });
 }
